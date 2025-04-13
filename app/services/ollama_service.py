c78ca658a379f2aac8359012fc69060437d7c391
Ahmed Hosny <ahosny@brightskiesinc.local>
2025-04-13 15:54:55 +0200

    deafault model

--- app/services/ollama_service.py
+++ app/services/ollama_service.py
@@ -11,9 +11,7 @@ def analyze_image(image_path: str, model: str = settings.model_name) -> str:
         messages=[{
             'role': 'user',
             'content': (
-                "What is in this image? Please analyze the person in this image and start by giving a short natural language description of the person's appearance, "
-                "focusing especially on their clothing. Mention the type, material (thick or light), color, and any noticeable features or textures. "
-                "Include whether the person appears dressed for warm or cold weather.\n\n"
+                "What is in this image? Please analyze the person in this image and start by giving a long detailed natural language description of the person's appearance, focusing especially on their clothing. Mention the type, material (thick or light), color, and any noticeable features or textures. Include whether the person appears dressed for warm or cold weather.\n\n"
                 "After the description, please answer the following questions:\n"
                 "1. What type of clothing is the person wearing? (e.g., t-shirt, sweater, jacket, etc.)\n"
                 "2. Is the sleeve length short, long, or sleeveless?\n"
