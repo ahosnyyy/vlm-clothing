a8c42e782733c162bcd72da5da8ed81fffa0e2a7
Ahmed Hosny <ahosny@brightskiesinc.local>
2025-05-04 13:21:39 +0300

    Adding CLO value estimation

--- app/services/ollama_service.py
+++ app/services/ollama_service.py
@@ -15,6 +15,8 @@ def analyze_image(image_path: str, model: str = settings.model_name) -> str:
                 "You are an assistant that describes clothing in images. "
                 "You MUST NOT describe or speculate about the person's age. "
                 "Focus only on visible clothing, accessories, colors, materials, and thermal properties."
+                "The description MUST BE at least 4 sentences."
+                
             )
             },
             {
